bd80fcd3f8fc1a544e860c3e4932fd5e6347643a
Flynn Stevens <flynnstevens30@gmail.com>
2023-12-08 14:45:40 +1300

    Start project, basic file setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000 - http://localhost:3000/')
+})
